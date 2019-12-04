1837b8dcda6646436650feb350314a4f6f3d5171
Frederic Heem <frederic.heem@gmail.com>
2019-12-04 20:35:09 +0000

    Updates

---
+++ precache-manifest.4bc1f4290783f0b43e78776e42c71268.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4683976e8bf511af08d7035fb144bdae",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "567433f0296e1588e34e",
+    "url": "/cv/static/css/main.27bf4f33.chunk.css"
+  },
+  {
+    "revision": "d2aedc1f08eefb03f81a",
+    "url": "/cv/static/js/2.6f72eab0.chunk.js"
+  },
+  {
+    "revision": "567433f0296e1588e34e",
+    "url": "/cv/static/js/main.0f296cd8.chunk.js"
+  },
+  {
+    "revision": "c33f9401843fc80c7893",
+    "url": "/cv/static/js/runtime-main.7a636a67.js"
+  },
+  {
+    "revision": "58185b305827c6707f362fe7623245b9",
+    "url": "/cv/static/media/email.58185b30.svg"
+  },
+  {
+    "revision": "7994930a7abc67185c3fbdc41ccba7a4",
+    "url": "/cv/static/media/location.7994930a.svg"
+  }
+]);
